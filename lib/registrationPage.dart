130f42f98e16073a7167179bb2185549bd4fef3d
ashu3103 <ash2003sharma@gmail.com>
2023-03-03 12:47:45 +0530

    Register Page

--- lib/registrationPage.dart
+++ lib/registrationPage.dart
@@ -1,10 +1,10 @@
+// import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'utils/components.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
-
-
 
 class RegistrationPage extends StatefulWidget {
   const RegistrationPage({Key? key}) : super(key: key);
@@ -15,16 +15,21 @@ class RegistrationPage extends StatefulWidget {
 
 String username = '';
 String password = '';
+String role = '';
+bool show = false;
+String public_key_driver = '';
 bool formCompleted = false;
+late String phone;
 
 class _RegistrationPageState extends State<RegistrationPage> {
-  void validateForm (){
-    if(username.length != 0 && password.length > 5){
-      formCompleted =true;
-    }else{
+  void validateForm() {
+    if (username.length != 0 && password.length > 5 && phone.length!=10 && (role=='Rider' || (role=='Driver' && public_key_driver!=''))) {
+      formCompleted = true;
+    } else {
       formCompleted = false;
     }
   }
+
   bool signupObscure = true;
   // FirebaseAuth _auth = FirebaseAuth.instance;
   bool showSpinner = false;
@@ -40,79 +45,183 @@ class _RegistrationPageState extends State<RegistrationPage> {
               children: [
                 HeadClipper(),
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Username', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)),
-                      TextField(
-                        onChanged: (val){
-                          username = val;
-                          setState(() {validateForm();});
-                        },
-                        cursorColor: Colors.black,
-                        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-                        decoration: customBoxStyle.copyWith(
-                          hintText: 'Enter your e-mail id'
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Username',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16.0)),
+                        TextField(
+                          onChanged: (val) {
+                            username = val;
+                            setState(() {
+                              validateForm();
+                            });
+                          },
+                          cursorColor: Colors.black,
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                          decoration: customBoxStyle.copyWith(
+                              hintText: 'Enter your e-mail id'),
                         ),
-                      ),
-                      SizedBox(height: 15.0),
-                      Text('Password', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)),
-                      TextField(
-                        onChanged: (val){
-                          password = val;
-                          setState(() {validateForm();});
-                        },
-                        cursorColor: Colors.black,
-                        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-                        obscureText: signupObscure,
-                        decoration: customBoxStyle.copyWith(
-                          suffixIcon: IconButton(
-                            icon: Icon(Icons.remove_red_eye, color: Colors.grey),
-                            onPressed: (){
+                        SizedBox(height: 15.0),
+                        Text('Password',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16.0)),
+                        TextField(
+                          onChanged: (val) {
+                            password = val;
+                            setState(() {
+                              validateForm();
+                            });
+                          },
+                          cursorColor: Colors.black,
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                          obscureText: signupObscure,
+                          decoration: customBoxStyle.copyWith(
+                              suffixIcon: IconButton(
+                                icon: Icon(Icons.remove_red_eye,
+                                    color: Colors.grey),
+                                onPressed: () {
+                                  setState(() {
+                                    signupObscure =
+                                        signupObscure == true ? false : true;
+                                  });
+                                },
+                              ),
+                              hintText: 'Minimum 6 characters requires'),
+                        ),
+                        SizedBox(height: 15.0),
+                        Column(
+                          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            ListTile(
+                              title: const Text('Rider'),
+                              leading: Radio(
+                                value: "Rider",
+                                groupValue: "Role",
+                                onChanged: (value) {
+                                  setState(() {
+                                    role = value!;
+                                    show = false;
+                                  });
+                                },
+                              ),
+                            ),
+                            ListTile(
+                              title: const Text('Driver'),
+                              leading: Radio(
+                                value: "Driver",
+                                groupValue: "Role",
+                                onChanged: (value) {
+                                  setState(() {
+                                    role = value!;
+                                    show = true;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 15.0),
+                        if (show)
+                          Text('Public Key of Driver',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16.0)),
+                        if (show)
+                          TextField(
+                            onChanged: (val) {
+                              password = val;
                               setState(() {
-                                signupObscure = signupObscure == true ? false : true;
+                                validateForm();
                               });
                             },
+                            cursorColor: Colors.black,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                            obscureText: signupObscure,
+                            decoration: customBoxStyle.copyWith(
+                                suffixIcon: IconButton(
+                                  icon: Icon(Icons.remove_red_eye,
+                                      color: Colors.grey),
+                                  onPressed: () {
+                                    setState(() {
+                                      signupObscure =
+                                          signupObscure == true ? false : true;
+                                    });
+                                  },
+                                ),
+                                hintText: 'Minimum 6 characters requires'),
                           ),
-                            hintText: 'Minimum 6 characters requires'
-                        ),
-                      ),
-                      SizedBox(height: 15.0),
-                      GestureDetector(
-                        onTap: () async{
-                          if(formCompleted){
+                        if (show) SizedBox(height: 15.0),
+                        Text('Phone Number',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16.0)),
+                        TextField(
+                          onChanged: (val) {
                             setState(() {
-                              showSpinner = true;
+                              phone = val;
+                              validateForm();
                             });
-                            try{
-                              // final newUser = await _auth.createUserWithEmailAndPassword(email: username, password: password);
-                              // if(newUser != null){
+                          },
+                          cursorColor: Colors.black,
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                          obscureText: signupObscure,
+                          decoration: customBoxStyle.copyWith(
+                              suffixIcon: IconButton(
+                                icon: Icon(Icons.remove_red_eye,
+                                    color: Colors.grey),
+                                onPressed: () {
+                                  setState(() {
+                                    signupObscure =
+                                        signupObscure == true ? false : true;
+                                  });
+                                },
+                              ),
+                              hintText: 'Minimum 10 characters requires'),
+                        ),
+                        SizedBox(height: 15.0),
+                        GestureDetector(
+                          onTap: () async {
+                            if (formCompleted) {
+                              setState(() {
+                                showSpinner = true;
+                              });
+                              try {
+                                // final newUser = await _auth.createUserWithEmailAndPassword(email: username, password: password);
+                                // if(newUser != null){
                                 Navigator.pushNamed(context, '/dashboard');
                                 setState(() {
                                   showSpinner = false;
                                 });
-                              // }
-                            }catch(e){
-                              print(e);
+                                // }
+                              } catch (e) {
+                                print(e);
+                              }
+                            } else {
+                              null;
                             }
-                          }else{
-                            null;
-                          }
-                        },
-                        child: Container(
-                          alignment: Alignment.center,
-                          width: double.infinity,
-                          padding: EdgeInsets.symmetric(vertical: 10.0),
-                          margin: EdgeInsets.symmetric(horizontal: 15.0),
-                          child: Text('Sign up', style: TextStyle(fontSize: 20.0, color: Colors.white)),
-                          decoration: BoxDecoration(
-                              color: formCompleted ? Color(0xff14279B) : Color(0xff14279B).withOpacity(0.5)
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            width: double.infinity,
+                            padding: EdgeInsets.symmetric(vertical: 10.0),
+                            margin: EdgeInsets.symmetric(horizontal: 15.0),
+                            child: Text('Sign up',
+                                style: TextStyle(
+                                    fontSize: 20.0, color: Colors.white)),
+                            decoration: BoxDecoration(
+                                color: formCompleted
+                                    ? Color(0xff14279B)
+                                    : Color(0xff14279B).withOpacity(0.5)),
                           ),
                         ),
-                      ),
-                    ]
-                  ),
+                      ]),
                 )
               ],
             ),
@@ -121,5 +230,4 @@ class _RegistrationPageState extends State<RegistrationPage> {
       ),
     );
   }
-
 }
